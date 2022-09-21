1b5f1f23e8b90f4c33d5f65462676e7080d665b1
Gun woo Lee <gwlee412@gmail.com>
2022-09-21 16:43:16 -0700

    added button for mobile

--- index.js
+++ index.js
@@ -78,6 +78,35 @@ xhttp.setRequestHeader('Authorization', '563492ad6f9170000100000196aa2a95555e4d4
 xhttp.send();
 })
 
+const button = document.querySelector('#go')
+button.addEventListener('click', function(e){
+    e.preventDefault();
+    var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    const container = document.querySelector('.container')
+    const imageBase = JSON.parse(xhttp.responseText);
+    const images =imageBase.photos
+    container.innerHTML=''
+       for(const image of images){
+        console.log(image)
+        const imageContainer = document.createElement('div');
+        imageContainer.classList.add('imageContainer');
+        imageContainer.innerHTML=`
+            <img src = ${image.src.portrait}>
+            <h4>${image.alt}</h4>
+        `;
+        container.appendChild(imageContainer)
+       }
+       
+    }
+};
+const textValue = document.querySelector('#search-bar').value
+console.log(textValue)
+xhttp.open("GET", `https://api.pexels.com/v1/search?query=${textValue}&per_page=8`, true);
+xhttp.setRequestHeader('Authorization', '563492ad6f9170000100000196aa2a95555e4d408d4e4222c7b58079');
+xhttp.send();
+})
 
 
 
